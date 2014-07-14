1ba4eaf1e08d46b1ad89af3f6ab2150cb6cc62e4
vitiaaap29 <viktor.kyrilovich@gmal.com>
2014-07-14 11:56:53 +0300

    5 Map

--- funcs.js
+++ funcs.js
@@ -2,11 +2,26 @@
 Problem 3: Linear fold 
 */
 
-function ForEach(array, callback,initialValue){
+function forEach(array, callback,initialValue){
 	var previosValue = initialValue;
 
 	for (var index = 0; index <array.length; index++) {
 		var currentValue = array[index];
 		previosValue = callback(previosValue, currentValue, index, array);
 	};
+}
+
+
+/**
+Problem 5: Map 
+*/
+
+function map (array, func) {
+	var newArray = [];
+
+	for (var i = 0; i < array.length; i++){
+		newArray.pop(func(array[i]));
+	}
+
+	return newArray;
 }
