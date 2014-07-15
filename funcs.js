c0d32bb88c91d308d00d59b4192d345bc1092df1
viktor-punko <punko.viktor@yandex.ru>
2014-07-15 11:54:59 +0300

    8 Sum of random numbers

--- funcs.js
+++ funcs.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
 Problem 3: Linear fold 
 */
@@ -45,12 +47,55 @@ function filter(array, filterCallback){
 }
 
 /**
-Problem 7: Average of even numbers 
+Services sum function
 */
-function calculateAverage(array){
+function sum(array){
 	var sum = 0;
 	linearFold(array, function(previosValue,currentValue,index, array){
 		sum += currentValue;
 	}, array[0]);
 	return sum;
+};
+
+/**
+Problem 7: Average of even numbers
+*/
+function calculateAverage(array){
+	return sum(array)/array.length;
+}
+
+/**
+Problem 8: Sum of random numbers 
+*/
+function sumOfRandomNumbers(count){
+	
+	// http://stackoverflow.com/questions/18082/validate-decimal-numbers-in-javascript-isnumeric
+	function isNumber(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+
+	if (count < 0 && isNumber(count))
+	{
+		return;
+	}
+	// copy paste from here: http://javascript.ru/Math.random
+	var generateRandomNumber = function (min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	var generateRandomArray = function (size) {
+		var MAX_RANDOM_NUMBER = 1000000000;
+		var MIN_RANDOM_NUMBER = 1;
+
+		var randomArray = [];
+
+		for (var i = 0; i < size; i++){
+			randomArray.push(generateRandomNumber(MIN_RANDOM_NUMBER,
+				MAX_RANDOM_NUMBER));
+		}
+
+		return randomArray;
+	};
+
+	return sum(generateRandomArray(count));
 }
